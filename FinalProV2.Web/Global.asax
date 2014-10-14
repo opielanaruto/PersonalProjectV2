764315e2266cf071c994a2f96c2c4e0baf28b1ae
opielanaruto <opielanaruto@gmail.com>
2014-10-14 14:19:01 -0500

    Initial

---
+++ FinalProV2.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProV2.Web.MvcApplication" Language="C#" %>
---
+++ FinalProV2.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalProV2.Web.Startup))]
+namespace FinalProV2.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
