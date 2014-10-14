98943b3a6b974c8d4fae3e4596caeb4572340f3f
opielanaruto <opielanaruto@gmail.com>
2014-10-14 14:32:30 -0500

    Added Migrations

---
+++ FinalProV2.Db/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace FinalProV2.Db.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FinalProV2.Db.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FinalProV2.Db.ApplicationDbContext context)
+        {
+			//Seeder.Seed(context);
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
